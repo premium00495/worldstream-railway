703991db7eea4de18aef6b53d20faca5a7485a17
premium00495 <premium00495@gmail.com>
2026-02-20 21:14:25 +0500

    Update server.js

--- server.js
+++ server.js
@@ -5,19 +5,18 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// IMPORTANT: API route static se pehle
-app.get('/api/streams', (req, res) => {
-    const data = fs.readFileSync(path.join(__dirname, 'streams'), 'utf8');
-
-    res.setHeader('Content-Type', 'application/json');
-    res.send(JSON.stringify({
-        success: true,
-        urls: JSON.parse(data)
-    }));
-});
-
-// Static files baad me
 app.use(express.static(__dirname));
+
+app.get('/api/streams', (req, res) => {
+    const streams = JSON.parse(
+        fs.readFileSync(path.join(__dirname, 'streams.json'))
+    );
+
+    res.json({
+        success: true,
+        urls: streams
+    });
+});
 
 app.listen(PORT, () => {
     console.log("Server running on port " + PORT);
